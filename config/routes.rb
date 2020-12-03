885220a6db765bb9899825b9f483304a74f74bda
devludaley <daleyluis@gmail.com>
2020-12-03 14:29:21 -0500

    create namespace in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  resources :users
+  # post "/api/v1/login", to: "api/v1/sessions#create"
+  # post "/api/v1/signup", to: "api/v1/users#create"
+  # delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  # get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  # I want my api be globetrotter.com/api/v1/resource
+  namespace :api do
+    namespace :v1 do
+      # resources :visits
+      # resources :trips
+      # resources :comments
+      # resources :attractions
+      # resources :locations
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
