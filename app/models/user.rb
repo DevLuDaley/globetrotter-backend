9a563c6b65af0046ae2a097e1fa13fab226e000f
devludaley <daleyluis@gmail.com>
2020-12-04 15:35:04 -0500

    Update user.rb
    add `optional: true` to belongs_to assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-    has_secure_password
+  has_secure_password
+  belongs_to :hometown, class_name: "Location", optional: true
+# ! uncommented to test serializer
+  has_many :trips
+  has_many :comments
+  has_many :visits, through: :trips
+  has_many :attractions, through: :trips
+  has_many :locations, through: :trips
 
-    belongs_to :hometown, class_name: "Location"
-#   has_many :trips
-#   has_many :visits, through: :trips
-#   has_many :attractions, through: :trips
-#   has_many :locations, through: :trips
-#   has_many :comments
-
+  def daley
+    self.trips.map { |trip| {name: trip.name}}
+    # "hi Lu!"
+  end
 #   validates :name, :username, :hometown, presence: true
 #   validates :username, uniqueness: true
 end
