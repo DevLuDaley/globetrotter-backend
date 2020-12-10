3ab27b2bed76e474155ce2f73f4a037a6683dc1a
devludaley <daleyluis@gmail.com>
2020-12-10 15:43:17 -0500

    add 'id' back to application_ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::API
-    #  include ::ActionController::Cookies
+  include ::ActionController::Cookies
 #  helpers do
         def current_user
-            # User.first
-            # User.find(session[:user_id])
-            User.find_by(session[:user_id])
+          # User.first
+          # User.find(session[:user_id])
+          User.find_by(id: session[:user_id])
         end
 
         def logged_in?
-            !!current_user
+          !!current_user
         end
 end
 # end
